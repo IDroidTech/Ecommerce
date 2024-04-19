3d829ca95ef2ba8ce1148a10e43f4919ff1e0caf
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-04-19 23:14:42 +0200

    Remove AppService Import Cause there is no need for it but keeping the file if needed

--- MVP/backend/src/app.controller.ts
+++ MVP/backend/src/app.controller.ts
@@ -1,13 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 import { ProductsService } from './products/services/products.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private products: ProductsService,
-  ) {}
+  constructor(private products: ProductsService) {}
 
   @Get()
   async getProducts() {
--- MVP/backend/src/app.module.ts
+++ MVP/backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ProductsModule } from './products/products.module';
 import { CategoryModule } from './category/category.module';
+import { SecurityModule } from './security/security.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ProductsModule, CategoryModule],
+  imports: [AuthModule, UserModule, ProductsModule, CategoryModule, SecurityModule],
   controllers: [AppController],
   providers: [AppService],
 })
