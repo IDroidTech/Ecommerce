eba6dbf4f57289e048c27915de7b77252db2fb6a
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-04-20 02:13:58 +0200

    Configure JWT

--- MVP/backend/src/auth/auth.module.ts
+++ MVP/backend/src/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { SecurityModule } from 'src/security/security.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule, SecurityModule],
+  imports: [
+    UserModule,
+    SecurityModule,
+    JwtModule.register({
+      secret: 'holberton-school-softwareEngineering-2024',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
--- MVP/backend/src/auth/services/auth.service.ts
+++ MVP/backend/src/auth/services/auth.service.ts
@@ -1,27 +1,30 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/services/user.service';
 import { SecurityService } from 'src/security/security.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     private userService: UserService,
     private securityService: SecurityService,
+    private jwtService: JwtService,
   ) {}
 
-  async login(data): Promise<string | Error> {
+  async login(data): Promise<{ access_token: string } | Error> {
     const user = await this.userService.getUser(data);
     if (user === null)
       throw new HttpException(
         'User Name Or Password Not Correct',
         HttpStatus.UNAUTHORIZED,
       );
+    const { password, ...userInfo } = user;
     const checkPassword = await this.securityService.comparePassword(
       data.password,
-      user.password,
+      password,
     );
     if (checkPassword) {
-      return `JWT Token`;
+      return { access_token: this.jwtService.sign(userInfo) };
     } else {
       throw new HttpException(
         'User Name Or Password Not Correct',
